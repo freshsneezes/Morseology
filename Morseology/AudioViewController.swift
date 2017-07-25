c68bb649b718239647a9ee9ab01de8a4f5429706
freshsneezes <freshsneezes@users.noreply.github.com>
2017-07-25 10:38:47 +0800

    Edited AudioViewController

--- Morseology/AudioViewController.swift
+++ Morseology/AudioViewController.swift
@@ -13,5 +13,21 @@ import AVFoundation
 class AudioViewController: UIViewController{
     var beep: AVAudioPlayer!
     
+    let path = Bundle.main.path(forResource: "beep.wav", ofType: nil)!
+    let url = URL(fileURLWithPath: <#T##String#>)
+    
+    do {
+        let sound = try AVAudioPlayer(contentsOf: url)
+        shortBeep = sound
+        sound.play()
+    } catch {
+        print ("Couldn't load file")
+    }
+    
+    /* To stop sound: 
+    if shortBeep )= nil {
+        shortBeep.stop()
+        shortBeep = nil
+    }
     
 }
