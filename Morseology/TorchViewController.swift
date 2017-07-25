c93ba5528747d136ed2bf175a06fc249230c3c11
freshsneezes <freshsneezes@users.noreply.github.com>
2017-07-25 09:55:29 +0800

    Wrote torch on/off function

--- Morseology/TorchViewController.swift
+++ Morseology/TorchViewController.swift
@@ -8,11 +8,34 @@
 
 import Foundation
 import UIKit
+import AVFoundation
 
 class TorchViewController: UIViewController{
     
     //To switch torch on
-    func switchOnTorch() {
+    func torchOn(on: Bool) {
+        guard let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo) else { return }
         
+        if device.hasTorch {
+            do {
+                try device.lockForConfiguration()
+                
+                if on == true {
+                    device.torchMode = .on
+                    print ("Torch is on")
+                } else {
+                    device.torchMode = .off
+                }
+                
+                device.unlockForConfiguration()
+            } catch {
+                print ("Torch could not be used")
+            }
+        } else {
+            print ("Torch is not available")
+        }
     }
+    
+    //To call torch function: torchOn(on: true)
+    
 }
