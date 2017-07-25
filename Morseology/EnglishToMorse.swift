42e683970880225acff6fb1fb848e9785dad107c
freshsneezes <freshsneezes@users.noreply.github.com>
2017-07-25 11:07:52 +0800

    Started the dictionary

---
+++ Morseology/EnglishToMorse.swift
@@ -0,0 +1,24 @@
+//
+//  EnglishToMorse.swift
+//  Morseology
+//
+//  Created by its on 25/7/2017.
+//  Copyright © 2017 Nerds In A Room. All rights reserved.
+//
+
+import Foundation
+
+class EnglishToMorse {
+    var englishToMorse: [String : String] = [
+        "A" : ".-",
+        "B" : "-...",
+        "C" : "-.-.",
+        "D" : "-..",
+        "E" : ".",
+        "F" : "..-.",
+        "G" : "--.",
+        "H" : "....",
+        "I" : "..",
+        "J" : ".---",
+    ]
+}
